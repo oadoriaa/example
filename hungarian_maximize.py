9bc4d52514613bd85693b2e05f98e6bf9990353c
Omar Doria <oadoriaa@gmail.com>
2026-01-19 13:22:48 +0100

    Add Hungarian algorithm implementation
    Implement Hungarian algorithm for max-sum assignment.

---
+++ hungarian_maximize.py
@@ -0,0 +1,57 @@
+def hungarian_maximize(cost):
+    """
+    Solve max-sum assignment for a square matrix using
+    Hungarian algorithm (O(n^3)).
+
+    Returns:
+        assignment: list where assignment[i] = assigned column for row i
+    """
+    n = len(cost)
+    u = [0] * (n + 1)
+    v = [0] * (n + 1)
+    p = [0] * (n + 1)
+    way = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        p[0] = i
+        j0 = 0
+        minv = [float("inf")] * (n + 1)
+        used = [False] * (n + 1)
+
+        while True:
+            used[j0] = True
+            i0 = p[j0]
+            delta = float("inf")
+            j1 = 0
+            for j in range(1, n + 1):
+                if not used[j]:
+                    cur = -(cost[i0 - 1][j - 1]) - u[i0] - v[j]
+                    if cur < minv[j]:
+                        minv[j] = cur
+                        way[j] = j0
+                    if minv[j] < delta:
+                        delta = minv[j]
+                        j1 = j
+            for j in range(n + 1):
+                if used[j]:
+                    u[p[j]] += delta
+                    v[j] -= delta
+                else:
+                    minv[j] -= delta
+            j0 = j1
+            if p[j0] == 0:
+                break
+
+        while True:
+            j1 = way[j0]
+            p[j0] = p[j1]
+            j0 = j1
+            if j0 == 0:
+                break
+
+    assignment = [0] * n
+    for j in range(1, n + 1):
+        if p[j] != 0:
+            assignment[p[j] - 1] = j - 1
+
+    return assignment
